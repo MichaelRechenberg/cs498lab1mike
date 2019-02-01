b263bcaae2819ab832836bec2456fd72f22f5a9a
Michael Rechenberg <rchnbg2@illinois.edu>
2019-01-31 20:02:52 -0600

    TESTED ON PI NOW...it works

--- driver.py
+++ driver.py
@@ -3,8 +3,10 @@
 #  and communicate with brake sensor and head unit
 import logging
 import os
+import math
 
 from picamera import PiCamera
+import cv2
 
 from picam_wrapper import PiCameraWrapper
 from object_recognition import ObjectRecognition
@@ -31,20 +33,26 @@ logger.setLevel(logging.INFO)
 logger.info("Initializing camera")
 format = 'rgb'
 camera = PiCamera()
-CAMERA_VERTICAL_RESOLUTION = 1280
 CAMERA_HORIZONTAL_RESOLUTION = 720
-camera.resolution = (CAMERA_HORIZONTAL_RESOLUTION, CAMERA_VERTICAL_RESOLUTION)
+CAMERA_VERTICAL_RESOLUTION = 1280
+camera.resolution = (CAMERA_VERTICAL_RESOLUTION, CAMERA_HORIZONTAL_RESOLUTION)
 
 # See PiCameraWrapper.capture_continuous() for what "array resolution" is
-CAPTURE_ARRAY_RESOLUTION = (CAMERA_HORIZONTAL_RESOLUTION, CAMERA_VERTICAL_RESOLUTION)
+CAPTURE_ARRAY_RESOLUTION = (CAMERA_VERTICAL_RESOLUTION, CAMERA_HORIZONTAL_RESOLUTION)
 
 # Init object recognition
+logger.info("Initializing ObjectRecognition object")
 # TODO: change getcwd() to env variable if we want
 MODEL_ROOT_DIR = os.path.join(os.getcwd(), "models")
 # Not using a quantized model at the moment since the quantized models 
 #   now use Tensorflow Lite and there isn't a pretty binary to use 
 #   for Tensorflow Lite on Pi (See Discord for more discussion)
 MODEL_NAME = "ssdlite_mobilenet_v2_coco_2018_05_09"
+
+# The minimum threshold to use for *detecting* objects. Note that this
+#  is not necessarily the threshold used for determining to send
+#  the brake signal or not
+SCORE_THRESHOLD = 0.2
 object_recognition_model = ObjectRecognition(MODEL_ROOT_DIR, MODEL_NAME)
 
 
@@ -68,23 +76,29 @@ RESIZED_VERTICAL_RESOLUTION = 300
 logger.info("Starting continous camera capture")
 while True:
 
-    logging.info("Capturing image")
+    logger.info("Capturing image")
     raw_capture_arr = PiCameraWrapper.capture(
         camera,
         CAPTURE_ARRAY_RESOLUTION,
         format
     )
 
+   
+
     resized_capture_arr = cv2.resize(
-        capture_arr,
+        raw_capture_arr,
         (RESIZED_HORIZONTAL_RESOLUTION, RESIZED_VERTICAL_RESOLUTION),
         interpolation=cv2.INTER_LINEAR
     )
+
+    logger.info("Performing inference")
 
     annotated_image, valid_classes, valid_scores = object_recognition_model.detect_objects(
             resized_capture_arr,
             SCORE_THRESHOLD
     )
+
+    logger.info("We performed inference")
 
     # TODO: Send annotated image and classes/scores to head unit
     # TODO: Send brake light signal to brake unit, receive image data and make decision based on that
